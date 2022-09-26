aa2606a39725902edf32560f6db104d304b7b901
Andrew <andrewhutchison1@users.noreply.github.com>
2022-09-26 21:22:42 +1000

    implement send expression

--- grammar.js
+++ grammar.js
@@ -185,6 +185,7 @@ module.exports = grammar({
         _binary_op: $ => choice(
             $.define_expression,
             $.assign_expression,
+            $.send_expression,
             $.lt_expression,
             $.gt_expression,
             $.eq_expression,
@@ -211,6 +212,7 @@ module.exports = grammar({
 
         define_expression: $ => infix_binary_op($, ':=', PREC.ASSIGNMENT, {lhs: $._pattern}),
         assign_expression: $ => infix_binary_op($, '=',  PREC.ASSIGNMENT, {assoc: 'R'}),
+        send_expression: $ => infix_binary_op($, '<-', PREC.CALL),
 
         lt_expression:  $ => infix_binary_op($, '<',    PREC.RELATIONAL),
         gt_expression:  $ => infix_binary_op($, '>',    PREC.RELATIONAL),
